145632a8e47dcdd5764927e5445547f631fb7907
Rtyota Niikura <ryota.niikura@nijibox.co.jp>
2017-06-22 18:23:23 +0900

    定数項を修正

--- src/mylibs/constants.py
+++ src/mylibs/constants.py
@@ -11,12 +11,12 @@ samples = 32 #2 ^5 +1
 batch_num = samples * 160
 span = batch_num + 100
 dims = 4 * EN
-neuron = 256
-epochs = 20
+neuron = 512
+epochs = 10
 
 # フーリエのよくわからない因子
 fourier_factor = 10
 inverse_fourier_factor = 10
 
 # モデル名
-model_name = '/data/model/mcreator15'
+model_name = '/data/model/mcreator16'
